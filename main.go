efc4cffb639653702180e5f8b09330e63666f8df
Kees <me@keestucker.com>
2023-01-15 17:43:39 +1300

    started integrating with mongodb + constants

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -20,6 +21,10 @@ type Comment struct {
 	Time    time.Time          `json:"_time,omitempty" bson:"time"`
 	Comment string             `json:"comment" bson:"comment"`
 }
+
+const RESTFULAPI_PORT = "1313"
+const FORUM_DATABASE_NAME = "forum"
+const COMMENT_COLLECTION_NAME = "commends"
 
 var client *mongo.Client
 var comments []Comment
@@ -46,19 +51,37 @@ func main() {
 	router := mux.NewRouter()
 
 	// Route handles & endpoints
-	router.HandleFunc("/comments", GetComments).Methods("GET")
-	router.HandleFunc("/comments", CreateComment).Methods("POST")
-	router.HandleFunc("/comments/{id}", GetComment).Methods("GET")
-	router.HandleFunc("/comments/{id}", UpdateComment).Methods("PATCH")
-	router.HandleFunc("/comments/{id}", DeleteComment).Methods("DELETE")
+	router.HandleFunc("/"+COMMENT_COLLECTION_NAME, GetComments).Methods("GET")
+	router.HandleFunc("/"+COMMENT_COLLECTION_NAME, CreateComment).Methods("POST")
+	router.HandleFunc("/"+COMMENT_COLLECTION_NAME+"/{id}", GetComment).Methods("GET")
+	router.HandleFunc("/"+COMMENT_COLLECTION_NAME+"/{id}", UpdateComment).Methods("PATCH")
+	router.HandleFunc("/"+COMMENT_COLLECTION_NAME+"/{id}", DeleteComment).Methods("DELETE")
 
 	// Start server
-	log.Fatal(http.ListenAndServe(":1313", router))
+	log.Fatal(http.ListenAndServe(":"+RESTFULAPI_PORT, router))
 }
 
 // GetComments retrieves all comments
 func GetComments(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get All")
+
+	// Define the collection
+	collection := client.Database(FORUM_DATABASE_NAME).Collection(COMMENT_COLLECTION_NAME)
+
+	// Find all documents in the collection
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var comments []Comment
+	// Iterate through the cursor to retrieve all documents
+	for cursor.Next(context.TODO()) {
+		var comment Comment
+		cursor.Decode(&comment)
+		comments = append(comments, comment)
+	}
+
 	json.NewEncoder(w).Encode(comments)
 }
 
@@ -66,13 +89,23 @@ func GetComments(w http.ResponseWriter, r *http.Request) {
 func GetComment(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get")
 	params := mux.Vars(r)
-	for _, comment := range comments {
-		if comment.ID.String() == params["id"] {
-			json.NewEncoder(w).Encode(comment)
-			return
-		}
+
+	// Convert the ID string to a MongoDB ObjectID
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	// Create a filter to find the specific comment
+	filter := bson.M{"_id": id}
+
+	// Find the comment in the "comments" collection
+	var result Comment
+	err := client.Database(FORUM_DATABASE_NAME).Collection(COMMENT_COLLECTION_NAME).FindOne(context.TODO(), filter).Decode(&result)
+
+	if err != nil {
+		// Return an empty comment if the ID is not found
+		json.NewEncoder(w).Encode(&Comment{})
+	} else {
+		json.NewEncoder(w).Encode(result)
 	}
-	json.NewEncoder(w).Encode(&Comment{})
 }
 
 // CreateComment creates a new comment
@@ -82,7 +115,14 @@ func CreateComment(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&comment)
 	comment.ID = primitive.NewObjectID()
 	comment.Time = time.Now()
-	comments = append(comments, comment)
+
+	// Add the comment to the MongoDB database
+	collection := client.Database(FORUM_DATABASE_NAME).Collection(COMMENT_COLLECTION_NAME)
+	_, err := collection.InsertOne(context.TODO(), comment)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	json.NewEncoder(w).Encode(comment)
 }
 
@@ -93,7 +133,7 @@ func UpdateComment(w http.ResponseWriter, r *http.Request) {
 	var comment Comment
 	_ = json.NewDecoder(r.Body).Decode(&comment)
 	for i, c := range comments {
-		if c.ID.String() == params["id"] {
+		if c.ID.Hex() == params["id"] {
 			comment.ID = c.ID
 			comments[i] = comment
 			json.NewEncoder(w).Encode(comment)
@@ -108,7 +148,7 @@ func DeleteComment(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete")
 	params := mux.Vars(r)
 	for i, c := range comments {
-		if c.ID.String() == params["id"] {
+		if c.ID.Hex() == params["id"] {
 			comments = append(comments[:i], comments[i+1:]...)
 			break
 		}
